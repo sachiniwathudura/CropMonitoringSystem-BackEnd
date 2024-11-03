3b36e8e2cd7da9e1ab3a364ddfbbf0123a63979a
sachiniwathudura <sachiniwathudura@gmail.com>
2024-11-03 22:38:31 +0530

    create StaffEntity

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/StaffEntity.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/StaffEntity.java
@@ -1,4 +1,38 @@
 package lk.ijse.gdse68.CropMonitoringSystem.entity;
 
-public class StaffEntity {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Staff")
+public class StaffEntity implements SuperEntity{
+    @Id
+    private String staffId;
+    private String firstName;
+    private String lastName;
+    private String designation;
+    private LocalDate joinDate;
+    private LocalDate DOB;
+    private String buildingNo;
+    private String lane;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String contactNo;
+    private String email;
+
+    @ManyToMany(mappedBy = "staff")
+    private List<FieldEntity> fields;
+
+    @OneToOne
+    @JoinColumn(name = "vehicle_code") // Add JoinColumn annotation
+    private VehicleEntity allocatedVehicle;
 }
