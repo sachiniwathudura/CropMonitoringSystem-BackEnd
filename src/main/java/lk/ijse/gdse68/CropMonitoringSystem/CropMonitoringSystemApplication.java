8537678472c7257e02432bfa456a00ae87a6325a
sachiniwathudura <sachiniwathudura@gmail.com>
2024-11-03 23:09:24 +0530

    create object- model mapper

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/CropMonitoringSystemApplication.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/CropMonitoringSystemApplication.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse68.CropMonitoringSystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CropMonitoringSystemApplication {
@@ -10,4 +12,8 @@ public class CropMonitoringSystemApplication {
 		SpringApplication.run(CropMonitoringSystemApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
