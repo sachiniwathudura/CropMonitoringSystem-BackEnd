71934b91f2150a8d28f8274eaa04ca7e289e0dd8
sachiniwathudura <sachiniwathudura@gmail.com>
2024-11-10 19:54:48 +0530

    update DataPersistFailedException

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/exception/DataPersistFailedException.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/exception/DataPersistFailedException.java
@@ -5,10 +5,10 @@ public class DataPersistFailedException extends RuntimeException {
     }
 
     public DataPersistFailedException(String message) {
-        super(message);
+
     }
 
     public DataPersistFailedException(String message, Throwable cause) {
-        super(message, cause);
+
     }
 }
