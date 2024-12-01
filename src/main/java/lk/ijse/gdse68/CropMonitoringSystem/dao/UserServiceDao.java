e8a87c5c725e63ae1823223b15db0e5051c2a91f
sachiniwathudura <sachiniwathudura@gmail.com>
2024-12-01 15:25:28 +0530

    implement UserServiceDao

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dao/UserServiceDao.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dao/UserServiceDao.java
@@ -3,5 +3,9 @@ package lk.ijse.gdse68.CropMonitoringSystem.dao;
 import lk.ijse.gdse68.CropMonitoringSystem.entity.UserServiceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserServiceDao extends JpaRepository<UserServiceEntity,String> {
+    Optional<UserServiceEntity> findByEmail(String email);
+
 }
