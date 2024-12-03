bc342b481d1225d9b89a93922488d2bd24f1b1d9
sachiniwathudura <sachiniwathudura@gmail.com>
2024-12-04 00:20:40 +0530

    updated toBase64()

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/util/AppUtil.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/util/AppUtil.java
@@ -2,41 +2,54 @@ package lk.ijse.gdse68.CropMonitoringSystem.util;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Base64;
 import java.util.UUID;
 
 public class AppUtil {
-    public static String createCropId(){
-        return "Crop- "+ UUID.randomUUID();
+//    public static String createCropId(){
+//        return "Crop- "+ UUID.randomUUID();
+//    }
+//    public static String createFieldCode(){
+//        return  "Field- "+ UUID.randomUUID();
+//    }
+//    public static String createEquipmentId(){
+//        return "Equipment- "+ UUID.randomUUID();
+//    }
+//
+//    public static String createMonitoringLogId(){
+//        return "MLog- "+ UUID.randomUUID();
+//    }
+//
+//    public static String createStaffId(){
+//        return "Staff- "+ UUID.randomUUID();
+//    }
+//
+//    public static String createVehicleCode(){
+//        return  "Vehicle- "+ UUID.randomUUID();
+//    }
+//    public static String toBase64ProfilePic(MultipartFile profilePic){
+//        String proPicBase64=null;
+//        try{
+//            byte[] imageByteCollection= profilePic.getBytes();
+//            proPicBase64= Base64.getEncoder().encodeToString(imageByteCollection);
+//
+//        }catch(Exception e ){
+//            e.printStackTrace();
+//        }
+//        return proPicBase64;
+//    }
+public static String toBase64Image(MultipartFile file) {
+    try {
+        byte[] imageBytes = file.getBytes();
+        return Base64.getEncoder().encodeToString(imageBytes);
+    } catch (IOException e) {
+        e.printStackTrace();
+        return null;
     }
-    public static String createFieldCode(){
-        return  "Field- "+ UUID.randomUUID();
-    }
-    public static String createEquipmentId(){
-        return "Equipment- "+ UUID.randomUUID();
-    }
-
-    public static String createMonitoringLogId(){
-        return "MLog- "+ UUID.randomUUID();
-    }
-
-    public static String createStaffId(){
-        return "Staff- "+ UUID.randomUUID();
-    }
-
-    public static String createVehicleCode(){
-        return  "Vehicle- "+ UUID.randomUUID();
-    }
-    public static String toBase64ProfilePic(MultipartFile profilePic){
-        String proPicBase64=null;
-        try{
-            byte[] imageByteCollection= profilePic.getBytes();
-            proPicBase64= Base64.getEncoder().encodeToString(imageByteCollection);
-
-        }catch(Exception e ){
-            e.printStackTrace();
-        }
-        return proPicBase64;
+}
+    public static String toBase64ProfilePic(byte [] profilePic){
+        return Base64.getEncoder().encodeToString(profilePic);
     }
 
     public static String toBase64ProfilePic(String img2) {
