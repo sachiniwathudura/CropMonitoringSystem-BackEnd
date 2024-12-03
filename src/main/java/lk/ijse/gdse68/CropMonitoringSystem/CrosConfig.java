7a95972b7660e008bec06101a41c091c131d8f14
sachiniwathudura <sachiniwathudura@gmail.com>
2024-12-04 00:21:25 +0530

    create cross config

---
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/CrosConfig.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse68.CropMonitoringSystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CrosConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Use appropriate origins
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
